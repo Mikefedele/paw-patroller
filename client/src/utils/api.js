0f8fc4531775ed32fde644caf9e3531cb9f214fe
Mike Fedele <mikefid11@gmail.com>
2022-05-27 10:33:31 -0400

    cors

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -5,7 +5,7 @@ const CORS = "https://cors-anywhere.herokuapp.com/"
 // cors addded
 
 // add .env to client folder to hide API key
-
+// todo cors change
 export const searchYelpApi = (query) => {
   console.log('hitAPI')
     return fetch(`${CORS}https://api.yelp.com/v3/businesses/search?term=dogs_allowed&location=${query}`, {
