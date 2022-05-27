03846bd8be773149a48bf78b34c71a7ef5d1915d
Mike Fedele <mikefid11@gmail.com>
2022-05-27 10:27:49 -0400

    update

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,13 +1,13 @@
 // const yelp = require('yelp-fusion');
-// const CORS = "https://cors-anywhere.herokuapp.com/"
+const CORS = "https://cors-anywhere.herokuapp.com/"
 
 // Yelp needed cors-anywhere until deployed. Can update after deployment
 
 // add .env to client folder to hide API key
-// ${CORS}
+
 export const searchYelpApi = (query) => {
   console.log('hitAPI')
-    return fetch(`https://api.yelp.com/v3/businesses/search?term=dogs_allowed&location=${query}`, {
+    return fetch(`${CORS}https://api.yelp.com/v3/businesses/search?term=dogs_allowed&location=${query}`, {
       method: 'GET',
       headers: {
         authorization: process.env.REACT_APP_API_KEY,
