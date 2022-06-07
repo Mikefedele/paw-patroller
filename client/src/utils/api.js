8d283df65cfa8d4c1a896accad4204d81648671f
Mike Fedele <mikefid11@gmail.com>
2022-06-07 14:41:29 -0400

    update

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,5 +1,5 @@
 // const yelp = require('yelp-fusion');
-const CORS = "https://cors-anywhere.herokuapp.com/"
+// const CORS = "https://cors-anywhere.herokuapp.com/"
 
 
 // Yelp needed cors-anywhere until deployed. Can update after deployment
@@ -9,7 +9,7 @@ const CORS = "https://cors-anywhere.herokuapp.com/"
 // todo cors changed to cors-cure server set it to const for read-ability
 export const searchYelpApi = (query) => {
   console.log('hitAPI')
-    return fetch(`${CORS}https://api.yelp.com/v3/businesses/search?term=dogs_allowed&location=${query}`, {
+    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=dogs_allowed&location=${query}`, {
       method: 'GET',
       headers: {
         authorization: process.env.REACT_APP_API_KEY,
